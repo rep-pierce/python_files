65c956ba0bf1b48e52eccef5a96c0f33b3ec316d
Ryan Pierce <pierce.ryan.97@gmail.com>
2023-03-28 11:37:07 -0600

    initial commit

---
+++ pprint_test.py
@@ -0,0 +1,10 @@
+from pprint import pprint
+
+data = {
+    "name": "John",
+    "age": 30,
+    "skills": ["Python", "Java", "JavaScript"],
+    "address": {"street": "Main St", "city": "New York", "state": "NY"}
+}
+
+print(data)

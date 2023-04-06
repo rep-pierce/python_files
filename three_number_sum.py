a22f2960cb015ba4dc49e2a98edcbcc3cfbfdff0
Ryan Pierce <pierce.ryan.97@gmail.com>
2023-04-06 17:06:54 -0600

    update

--- three_number_sum.py
+++ three_number_sum.py
@@ -1,20 +1,40 @@
-def threeNumberSum(array, targetSum):
-    array.sort()
-    res = []
+# def threeNumberSum(array, targetSum):
+#     array.sort()
+#     res = []
     
-    for i in range(len(array)):
-        twoSum(array, i, res, targetSum)
-    return res
+#     for i in range(len(array)):
+#         twoSum(array, i, res, targetSum)
+#     return res
 
-def twoSum(array, i, res, targetSum):
-    l, r = i + 1, len(array) - 1
+# def twoSum(array, i, res, targetSum):
+#     l, r = i + 1, len(array) - 1
 
-    while l < r:
-        if array[i] + array[l] + array[r] < targetSum:
-            l += 1
-        elif array[i] + array[l] + array[r] > targetSum:
-            r -= 1
-        else:
-            res.append([array[i], array[l], array[r]])
-            l += 1
-    return res
+#     while l < r:
+#         if array[i] + array[l] + array[r] < targetSum:
+#             l += 1
+#         elif array[i] + array[l] + array[r] > targetSum:
+#             r -= 1
+#         else:
+#             res.append([array[i], array[l], array[r]])
+#             l += 1
+#     return res
+
+def threeNumberSum(array, targetSum):
+    foundTriplets = []
+    itemMap = {}
+    array.sort()
+
+    for i in array:
+        itemMap[i] = True
+
+    for i in array:
+        for j in array:
+            if i != j:
+                targetSumDelta = targetSum - (i + j)
+                if targetSum != i and targetSumDelta != j and targetSumDelta in itemMap:
+                    hit = [i, j, targetSumDelta]
+                    hit.sort()
+                    if not hit in foundTriplets:
+                        foundTriplets.append(hit)
+
+    return foundTriplets

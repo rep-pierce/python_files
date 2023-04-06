3775a8214b5bd6bcaeea12f455dd122948ae4abd
Ryan Pierce <pierce.ryan.97@gmail.com>
2023-04-06 17:09:47 -0600

    update

--- three_number_sum.py
+++ three_number_sum.py
@@ -19,22 +19,30 @@
 #             l += 1
 #     return res
 
+# def threeNumberSum(array, targetSum):
+#     foundTriplets = []
+#     itemMap = {}
+#     array.sort()
+
+#     for i in array:
+#         itemMap[i] = True
+
+#     for i in array:
+#         for j in array:
+#             if i != j:
+#                 targetSumDelta = targetSum - (i + j)
+#                 if targetSum != i and targetSumDelta != j and targetSumDelta in itemMap:
+#                     hit = [i, j, targetSumDelta]
+#                     hit.sort()
+#                     if not hit in foundTriplets:
+#                         foundTriplets.append(hit)
+
+#     return foundTriplets
+
 def threeNumberSum(array, targetSum):
-    foundTriplets = []
-    itemMap = {}
-    array.sort()
-
-    for i in array:
-        itemMap[i] = True
-
-    for i in array:
-        for j in array:
-            if i != j:
-                targetSumDelta = targetSum - (i + j)
-                if targetSum != i and targetSumDelta != j and targetSumDelta in itemMap:
-                    hit = [i, j, targetSumDelta]
-                    hit.sort()
-                    if not hit in foundTriplets:
-                        foundTriplets.append(hit)
-
-    return foundTriplets
+    lst = []
+    for num1 in array:
+        for num2 in array[array.index(num1) + 1:]:
+            if (targetSum - num1 - num2) in array[array.index(num2) + 1:]:
+                lst.append(sorted([num1, num2, targetSum - num1 - num2]))
+    return sorted(lst)
